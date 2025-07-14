434e0cc3346cb021dad070ec623bbe0ab3d09c44
yadu <kyadushree47@gmail.com>
2025-07-14 14:27:44 +0530

    to-do list

---
+++ JS/todo.js
@@ -0,0 +1,21 @@
+function addTask(){
+    const input = document.getElementById('taskInput');
+    const text = input.value.trim();
+
+    const container = document.getElementById('taskContainer')
+
+    const li = document.createElement('li');
+    li.textContent = text;
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'DELETE';
+
+    deleteBtn.addEventListener('click', ()=>{
+        li.remove();
+        
+    })
+
+    li.append(deleteBtn)
+    document.getElementById('taskContainer').append(li)
+    input.value =''
+}
