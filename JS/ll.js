1810384a025a99afd757c6f61fc7d724fc0773bb
yadu <kyadushree47@gmail.com>
2025-08-12 16:51:46 +0530

    linkedlist DSA

---
+++ JS/ll.js
@@ -0,0 +1,101 @@
+// Node constructor
+function Node(value){
+    this.value = value;
+    this.next = null;
+}
+// Manually create nodes and link them
+const n1 = new Node(10);
+const n2 = new Node(20);
+const n3 = new Node(30);
+
+// Link nodes: n1 -> n2 -> n3
+n1.next = n2;
+n2.next = n3
+
+const head = n1;
+
+function printFrom(head){
+    let curr = head;
+    let out = "";
+    while(curr){
+        out += curr.value + (curr.next ? " -> ": "");
+
+    }
+    console.log(out || "Empty list");
+    
+}
+
+// printFrom(head);
+
+class Node1{
+    constructor(value){
+        this.value = value;
+        this.next = null
+    }
+}
+
+class LinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    append(value){
+        const newNode = new Node(value);
+
+        if(!this.head){
+            this.head = newNode;
+            this.tail = newNode
+        }else{
+            this.tail.next = newNode;
+            this.tail = newNode
+        }
+        this.length++
+    }
+    prepend(value){
+        const newNode = new Node(value);
+
+        if(!this.head) {
+            this.head = newNode;
+            this.tail = newNode
+        }else{
+            newNode.next = this.head;
+            this.head = newNode
+        }
+        this.length++
+    }
+    printList(){
+        let curr = this.head;
+        let out = "";
+        while(curr){
+            out += curr.value + (curr.next ? " -> " :"");
+            curr = curr.next
+        }
+        console.log(out || "Emptylist");
+        return out
+    }
+    toArray(){
+        const res = [];
+        let curr = this.head;
+        while(curr){
+            res.push(curr.value);
+            curr = curr.next
+        }
+        return res
+    }
+}
+
+
+const list = new LinkedList();
+list.append(10)
+list.append(20)
+list.append(30)
+list.append(40)
+list.printList()
+
+list.prepend(5);
+list.printList()
+
+console.log(list.toArray());
+
+console.log(list.length);
